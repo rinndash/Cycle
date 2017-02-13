ced39b10b07238625ce001460d542bcdbec0e7bc
Jinseo Yoon <js.yoon@riiid.co>
2017-02-13 16:24:42 +0900

    initial commit

---
+++ Cycle/Cycle.h
@@ -0,0 +1,19 @@
+//
+//  Cycle.h
+//  Cycle
+//
+//  Created by jinseo on 2017. 2. 13..
+//  Copyright © 2017년 Riiid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cycle.
+FOUNDATION_EXPORT double CycleVersionNumber;
+
+//! Project version string for Cycle.
+FOUNDATION_EXPORT const unsigned char CycleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cycle/PublicHeader.h>
+
+
