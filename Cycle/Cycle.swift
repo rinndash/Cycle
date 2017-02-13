8af3333ba3764a2e6c0819f1b8e073cb114afca2
Jinseo Yoon <js.yoon@riiid.co>
2017-02-13 21:17:09 +0900

    changed Cycle to typealias

--- Cycle/Cycle.swift
+++ Cycle/Cycle.swift
@@ -9,20 +9,19 @@
 import Foundation
 import RxSwift
 
-public protocol Cycle {
-    static func cycle(_ sources: Sources) -> (Sinks, Disposable)
-}
+public typealias Cycle = (Sources) -> (Sinks, Disposable)
 
 public enum CycleKey {
     public static let view: String = "view"
 }
 
-public protocol CycleApp: Cycle {
+public protocol CycleApp {
+    associatedtype Dependency
     associatedtype Intent
     associatedtype Model
     associatedtype ViewModel
     
-    static func initialModel(from sources: Sources) -> Model
+    static func initialModel(from dependency: Dependency) -> Model
     static func intent(from sources: Sources, model$: Observable<Model>) -> Observable<Intent>
     static func update(model: Model, after intent: Intent) -> (Model, [SinkType])
     static func viewModel(from model: Model) -> ViewModel
@@ -30,29 +29,31 @@ public protocol CycleApp: Cycle {
 }
 
 public extension CycleApp {
-    static func cycle(_ sources: Sources) -> (Sinks, Disposable) {
-        let initModel = initialModel(from: sources)
-        let modelProxy = BehaviorSubject<Model>(value: initModel)
-        let intent$ = intent(from: sources, model$: modelProxy)
-        let model_effects = intent$
-            .scan((initModel, [])) { model_effect, intent -> (Model, [SinkType]) in
-                let (model, _) = model_effect
-                return update(model: model, after: intent)
+    static func app(_ dependency: Dependency) -> Cycle {
+        return { sources in
+            let initModel = initialModel(from: dependency)
+            let modelProxy = BehaviorSubject<Model>(value: initModel)
+            let intent$ = intent(from: sources, model$: modelProxy)
+            let model_effects = intent$
+                .scan((initModel, [])) { model_effect, intent -> (Model, [SinkType]) in
+                    let (model, _) = model_effect
+                    return update(model: model, after: intent)
+                }
+                .shareReplay(1)
+            
+            let subscription = model_effects.map { $0.0 }
+                .subscribe(modelProxy)
+            
+            let viewModel$ = modelProxy.map(viewModel)
+            let sinkType$ = model_effects.map { $0.1 }
+                .flatMap { Observable.from($0) }
+            
+            var sinks: Sinks = [:]
+            commandKeys.forEach { key in
+                sinks[key] = sinkType$.filter { $0.name == key }.castToAny()
             }
-            .shareReplay(1)
-        
-        let subscription = model_effects.map { $0.0 }
-            .subscribe(modelProxy)
-        
-        let viewModel$ = modelProxy.map(viewModel)
-        let sinkType$ = model_effects.map { $0.1 }
-            .flatMap { Observable.from($0) }
-        
-        var sinks: Sinks = [:]
-        commandKeys.forEach { key in
-            sinks[key] = sinkType$.filter { $0.name == key }.castToAny()
+            sinks[CycleKey.view] = viewModel$.castToAny()
+            return (sinks, subscription)
         }
-        sinks[CycleKey.view] = viewModel$.castToAny()
-        return (sinks, subscription)
     }
 }
